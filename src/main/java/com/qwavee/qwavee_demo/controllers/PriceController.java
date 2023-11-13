b51e42141f7d3abffa3562354e6d5aa3c4f25d6c
Ilari Nicolás <nilari@qwavee.com>
2023-11-13 03:34:18 -0300

    fixed collection json

--- src/main/java/com/qwavee/qwavee_demo/controllers/PriceController.java
+++ src/main/java/com/qwavee/qwavee_demo/controllers/PriceController.java
@@ -9,7 +9,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,7 +44,7 @@ public class PriceController {
     } else {
       PriceGetDto result = this.priceService.savePrice(price);
       if (result != null) {
-        return ResponseEntity.status(HttpStatus.OK).body(price);
+        return ResponseEntity.status(HttpStatus.CREATED).body(price);
       } else {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong");
       }
